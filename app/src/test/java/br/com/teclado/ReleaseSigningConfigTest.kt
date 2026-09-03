3b679addcfa01af25349f89433e942c949e9a836
João <contato@joaolds.xyz.br>
2026-09-03 19:05:52 -0300

    test: escape shell variables in signing regression

--- app/src/test/java/br/com/teclado/ReleaseSigningConfigTest.kt
+++ app/src/test/java/br/com/teclado/ReleaseSigningConfigTest.kt
@@ -28,7 +28,7 @@ class ReleaseSigningConfigTest {
 
     @Test
     fun releaseCertificateParsingDoesNotDependOnExactApksignerPrefix() {
-        assertTrue(workflow.contains("CERT_OUTPUT=$($APKSIGNER verify --print-certs \"$APK\")"))
+        assertTrue(workflow.contains("CERT_OUTPUT=\$(\$APKSIGNER verify --print-certs \"\$APK\")"))
         assertTrue(workflow.contains("grep -iE 'certificate.*sha-?256.*digest'"))
         assertFalse(workflow.contains("sed -n 's/^Signer #1 certificate SHA-256 digest: //p'"))
     }
